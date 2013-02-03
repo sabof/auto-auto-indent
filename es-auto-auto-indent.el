e2acc742a9a8ff8931a87dec1fcb83b5d9d1a825
sabof <sabof@example.com>
2013-02-03 23:22:07 +0000

    fix for error when pasting into org-mode

--- es-auto-auto-indent.el
+++ es-auto-auto-indent.el
@@ -122,7 +122,8 @@ Otherwise call `es-aai-indent-forward'."
                      es-aai-indented-yank-limit))
         (es-aai--indent-region starting-point (point)))
       ;; Necessary for web-mode. Possibly others
-      (when (bound-and-true-p font-lock-mode)
+      (when (and (bound-and-true-p font-lock-mode)
+                 (not (eq major-mode 'org-mode)))
         (font-lock-fontify-region starting-point (point)))
       (goto-line line)
       (goto-char (max (es-indentation-end-pos)
